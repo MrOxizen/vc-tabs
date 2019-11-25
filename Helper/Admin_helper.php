b678a420cf64583ca35a640b6993b23fa9c38324
Biplob Adhikari <50793821+MrOxizen@users.noreply.github.com>
2019-11-25 14:23:30 +0600

    asd

--- Helper/Admin_helper.php
+++ Helper/Admin_helper.php
@@ -60,7 +60,9 @@ trait Admin_helper {
                     <p></p>
                     <div class="oxi-addons-admin-notifications-holder">
                         <div class="oxi-addons-admin-notifications-alert">
-                            <p>Thank you for using my Responsive Tabs with Accordions. I Just wanted to see if you have any questions or concerns about my plugins. If you do, Please do not hesitate to <a href="https://wordpress.org/support/plugin/vc-tabs#new-post">file a bug report</a>. </p><p>By the way, did you know we also have a <a href="https://www.oxilab.org/downloads/responsive-tabs-with-accordions/">Premium Version</a>? It offers lots of options with automatic update. It also comes with 16/5 personal support.</p><p>Thanks Again!</p>
+                            <p>Thank you for using my Responsive Tabs with Accordions. I Just wanted to see if you have any questions or concerns about my plugins. If you do, Please do not hesitate to <a href="https://wordpress.org/support/plugin/vc-tabs#new-post">file a bug report</a>. </p>
+                            '.(apply_filters('oxi-tabs-plugin/pro_version', false) ? '': '<p>By the way, did you know we also have a <a href="https://www.oxilab.org/downloads/responsive-tabs-with-accordions/">Premium Version</a>? It offers lots of options with automatic update. It also comes with 16/5 personal support.</p>').'
+                            <p>Thanks Again!</p>
                             <p></p>
                         </div>                     
                     </div>
