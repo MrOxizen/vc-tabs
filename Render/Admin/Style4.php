dc1133583fea6eb7c1d120cbc125abf1f44ac7e8
Biplob Adhikari <50793821+MrOxizen@users.noreply.github.com>
2020-08-26 02:21:25 +0600

    done

---
+++ Render/Admin/Style4.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace OXI_TABS_PLUGINS\Render\Admin;
+
+/**
+ * Description of Effects1
+ *
+ * @author biplo
+ */
+use OXI_TABS_PLUGINS\Render\Helper;
+use OXI_TABS_PLUGINS\Render\Controls as Controls;
+
+class Style4 extends Helper {
+
+    public function register_header_general() {
+        $this->start_controls_section(
+                'oxi-tabs-head', [
+            'label' => esc_html__('Header General', OXI_TABS_TEXTDOMAIN),
+            'showing' => TRUE,
+                ]
+        );
+        $this->add_control(
+                'oxi-tabs-head-aditional-location', $this->style, [
+            'label' => __('Title Additional Location', OXI_TABS_TEXTDOMAIN),
+            'type' => Controls::SELECT,
+            'options' => [
+                'oxi-tab-header-aditional-left-position' => __('Left', OXI_TABS_TEXTDOMAIN),
+                'oxi-tab-header-aditional-top-position' => __('Top', OXI_TABS_TEXTDOMAIN),
+                'oxi-tab-header-aditional-right-position' => __('Right', OXI_TABS_TEXTDOMAIN),
+                'oxi-tab-header-aditional-bottom-position' => __('Bottom', OXI_TABS_TEXTDOMAIN),
+            ],
+            'selector' => [
+                '{{WRAPPER}} .oxi-tabs-header-li' => '',
+            ],
+            'description' => 'Set the Location of Title’s Additionals (Icon, Image, or Number.)',
+                ]
+        );
+
+        $this->add_control(
+                'oxi-tabs-head-bg', $this->style, [
+            'label' => __('Background', OXI_TABS_TEXTDOMAIN),
+            'type' => Controls::GRADIENT,
+            'default' => 'rgba(171, 0, 201, 1)',
+            'selector' => [
+                '{{WRAPPER}} .oxi-tabs-ultimate-style .oxi-tabs-ultimate-header-wrap' => 'background: {{VALUE}};',
+            ],
+            'description' => 'Set the Background of the Header on Normal Mode.',
+                ]
+        );
+        $this->add_group_control(
+                'oxi-tabs-head-header-shape', $this->style, [
+            'label' => __('Active Shape', OXI_TABS_TEXTDOMAIN),
+            'type' => Controls::SINGLEBORDER,
+            'selector' => [
+                '{{WRAPPER}}  .oxi-tabs-ultimate-style .oxi-tabs-header-shape' => 'border-left: {{SIZE}}px {{TYPE}} {{COLOR}};border-top: {{SIZE}}px {{TYPE}} {{COLOR}};'
+            ],
+            'description' => 'Customize Header Shape of the Header which Overlay Header Border during Active. Set Type, Size, and Color.',
+                ]
+        );
+
+        $this->add_group_control(
+                'oxi-tabs-head-header-divider', $this->style, [
+            'label' => __('Header Single Border', OXI_TABS_TEXTDOMAIN),
+            'type' => Controls::SINGLEBORDER,
+            'selector' => [
+                '{{WRAPPER}}  .oxi-tabs-ultimate-style .oxi-tabs-ultimate-header' => 'border: {{SIZE}}px {{TYPE}} {{COLOR}};;'
+            ],
+            'description' => 'Customize Header Single Border of the Header which divide with header. Set Type, Size, and Color.',
+                ]
+        );
+        $this->add_group_control(
+                'oxi-tabs-head-content-divider', $this->style, [
+            'label' => __('Content Divider', OXI_TABS_TEXTDOMAIN),
+            'type' => Controls::SINGLEBORDER,
+            'selector' => [
+                '{{WRAPPER}}  .oxi-tabs-ultimate-style .oxi-tabs-ultimate-header-wrap' => 'border: {{SIZE}}px {{TYPE}} {{COLOR}};'
+            ],
+            'description' => 'Customize Content Divider of the Header which divide with content. Set Type, Size, and Color.',
+                ]
+        );
+
+        $this->add_responsive_control(
+                'oxi-tabs-head-padding', $this->style, [
+            'label' => __('Padding', OXI_TABS_TEXTDOMAIN),
+            'type' => Controls::DIMENSIONS,
+            'default' => [
+                'unit' => 'px',
+                'size' => '',
+            ],
+            'range' => [
+                'px' => [
+                    'min' => 0,
+                    'max' => 500,
+                    'step' => 1,
+                ],
+                '%' => [
+                    'min' => 0,
+                    'max' => 100,
+                    'step' => 1,
+                ],
+                'em' => [
+                    'min' => 0,
+                    'max' => 100,
+                    'step' => .1,
+                ],
+            ],
+            'selector' => [
+                '{{WRAPPER}}  .oxi-tabs-ultimate-style .oxi-tabs-header-li' => 'padding:{{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+            ],
+            'description' => 'Generate some Space around the Header Content including background color.',
+                ]
+        );
+        $this->add_responsive_control(
+                'oxi-tabs-head-margin', $this->style, [
+            'label' => __('Margin', OXI_TABS_TEXTDOMAIN),
+            'type' => Controls::DIMENSIONS,
+            'default' => [
+                'unit' => 'px',
+                'size' => '',
+            ],
+            'range' => [
+                'px' => [
+                    'min' => 0,
+                    'max' => 500,
+                    'step' => 1,
+                ],
+                '%' => [
+                    'min' => 0,
+                    'max' => 100,
+                    'step' => 1,
+                ],
+                'em' => [
+                    'min' => 0,
+                    'max' => 100,
+                    'step' => .1,
+                ],
+            ],
+            'selector' => [
+                '{{WRAPPER}} .oxi-tabs-ultimate-style .oxi-tabs-ultimate-header' => 'margin:{{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+            ],
+            'description' => 'Create some Space outside of the Header Section.',
+                ]
+        );
+        $this->end_controls_section();
+    }
+
+}
