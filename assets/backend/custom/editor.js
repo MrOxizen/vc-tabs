521375a92d863437a38bd6cd21f593f549a3940f
Biplob Adhikari <50793821+MrOxizen@users.noreply.github.com>
2020-09-29 02:13:22 +0600

    update

--- assets/backend/custom/editor.js
+++ assets/backend/custom/editor.js
@@ -18,11 +18,6 @@ jQuery.noConflict();
             return elem.value.replace(/\r?\n/g, "");
         }
     };
-//    $.valHooks.textarea = {
-//        get: function (elem) {
-//            return elem.value.replace(/\\/g, "&#92;");
-//        }
-//    };
 
     function replaceStr(str, find, replace) {
         for (var i = 0; i < find.length; i++) {
